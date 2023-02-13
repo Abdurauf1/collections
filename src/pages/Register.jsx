7ffb10aad39f0fa3e890c82481c5d8a3bd0ae3af
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-02-14 02:00:11 +0500

    feat: creating form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 const Register = () => {
   return (
-    <>
-      <h1>Register</h1>
-    </>
+    <Form>
+      <Form.Group controlId="registerName">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control type="text" />
+      </Form.Group>
+      <Button type="submit">Submit</Button>
+    </Form>
   );
 };
 
