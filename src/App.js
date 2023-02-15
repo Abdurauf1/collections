5c3a0fe92c207c50de78ceec8ad01983a459b1de
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-02-16 02:33:49 +0500

    feat: add sign in sign up page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
