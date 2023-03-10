342f1ee01ba86fae19ae09682f1e2a9eb01b432c
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-03-10 18:04:00 +0500

    feat: created sign in form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import signUpImg from "../assets/images/signup.jpg";
+import SocialsSignIn from "../components/SocialsSignIn";
 
 const SignIn = () => {
   return (
-    <Container>
-      <Form className="w-50 my-5 mx-auto">
-        <Form.Group className="mb-3" controlId="logInEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control required />
-        </Form.Group>
-        <Form.Group className="my-3" controlId="logInPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control required />
-        </Form.Group>
-        <Button className="w-100">Log In</Button>
-      </Form>
+    <Container className="d-flex py-5 px-0">
+      <div className="w-50">
+        <h6 className="w-75 mb-5">
+          <span className="text-secondary">Not a member yet?</span>{" "}
+          <Link to="/signup" className="text-decoration-none">
+            Sign Up
+          </Link>
+        </h6>
+        <div className="w-75">
+          <h1 className="mb-4">Sign In</h1>
+          <Form>
+            <Form.Group className="mb-3" controlId="signInEmail">
+              <Form.Label>
+                Email <span className="text-danger">*</span>
+              </Form.Label>
+              <Form.Control type="email" name="email" required />
+            </Form.Group>
+            <Form.Group className="mb-5" controlId="signInPassword">
+              <Form.Label>
+                Password <span className="text-danger">*</span>
+              </Form.Label>
+              <Form.Control type="password" name="password" required />
+            </Form.Group>
+            <div className="d-flex align-items-center justify-content-between">
+              <Button type="submit">Sign In</Button>
+              <span className="text-secondary">or</span>
+              <SocialsSignIn />
+            </div>
+          </Form>
+        </div>
+      </div>
+      <img className="w-50" src={signUpImg} alt="sign up image" />
     </Container>
   );
 };
