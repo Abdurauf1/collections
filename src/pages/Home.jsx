7fbfc6ff5fc42757174054fdf42d5f64c3a3d629
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-03-10 18:03:01 +0500

    refactor: changed homepage to read only page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,9 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { api } from "../api/api";
-import { Table } from "react-bootstrap";
+import React from "react";
 
 const Home = () => {
-  const [users, setUsers] = useState([]);
-
-  const loadUsers = () => {
-    axios
-      .get(`${api}/users`)
-      .then(response => {
-        setUsers(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
-  useEffect(() => {
-    loadUsers();
-  }, []);
   return (
     <>
-      <Table>
-        <thead>
-          <tr>
-            <th style={{ textAlign: "center" }}>ID</th>
-            <th style={{ textAlign: "center" }}>Name</th>
-            <th style={{ textAlign: "center" }}>Email</th>
-            <th style={{ textAlign: "center" }}>Password</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(item => {
-            return (
-              <tr key={item.id}>
-                <td style={{ textAlign: "center" }}>{item.id}</td>
-                <td style={{ textAlign: "center" }}>{item.name}</td>
-                <td style={{ textAlign: "center" }}>{item.email}</td>
-                <td style={{ textAlign: "center" }}>{item.password}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      <h1>Read Only Home Page</h1>
     </>
   );
 };
